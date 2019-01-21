ee400181ad67cd39f267a25b5adbc12b895f0ec0
Blood-elf <1369466552@qq.com>
2019-01-21 22:02:54 +0800

    question

--- js/login.js
+++ js/login.js
@@ -1,5 +1,13 @@
-console.log("window height:" + $(window).height());
-console.log("window width:" + $(window).width());
+$(document).ready(function(){
+	var browerHeight = $(window).height();
+	var browerWidth = $(window).width();
+	var h1 = $(document.body).outerHeight();
+	var w1 = $(document.body).outerWidth();
+	console.log(browerWidth + " : "+ browerHeight);
+	console.log(h1 + " : " + w1);
+
+});
+
 //console.log("document height:" + $(document).height());
 //console.log("document.body height:"+ $(document.body).height());
 
