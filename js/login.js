a1864df69375119ee9ec95a8f45686f20b2b0227
Blood-elf <1369466552@qq.com>
2019-01-21 19:09:18 +0800

    first commit files

---
+++ js/login.js
@@ -0,0 +1,5 @@
+console.log("window height:" + $(window).height());
+console.log("window width:" + $(window).width());
+//console.log("document height:" + $(document).height());
+//console.log("document.body height:"+ $(document.body).height());
+
