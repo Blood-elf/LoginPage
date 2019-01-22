12ea07627dc3d2dc1f606418bc1d3552f956b4be
Blood-elf <1369466552@qq.com>
2019-01-22 21:56:53 +0800

    add practise file

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	var browerHeight = $(window).height();
 	var browerWidth = $(window).width();
+	console.log(browerWidth + " : "+ browerHeight);
 	var h1 = $(document.body).outerHeight();
 	var w1 = $(document.body).outerWidth();
-	console.log(browerWidth + " : "+ browerHeight);
 	console.log(h1 + " : " + w1);
-
+	
 });
 
 //console.log("document height:" + $(document).height());
